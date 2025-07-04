ff615d3695c99ebfd0d8701c90baccf5ea581fc6
TimonDeBeste <123480186+TimonDeBeste@users.noreply.github.com>
2025-07-04 13:22:50 +0200

    jose gejosed

--- scripts/jose.gd
+++ scripts/jose.gd
@@ -3,20 +3,26 @@ extends CharacterBody2D
 @export var speed : int = 256
 @export var jump_speed : int = 100
 @export var gravity : int = 25
+var done = false
 
 func _ready() -> void:
 	$AnimatedSprite2D.play("running")
 	$AnimatedSprite2D.scale = Vector2(3, 3)
 
 func _physics_process(_delta) -> void:
-	velocity.x = speed
-	velocity.y += gravity
-	if $RayCast2D.is_colliding():
-		velocity.y -= jump_speed
-	move_and_slide()
-	print(velocity.y)
+	if done == false:
+		velocity.x = speed
+		velocity.y += gravity
+		if $RayCast2D.is_colliding():
+			velocity.y -= jump_speed
+		move_and_slide()
+
 func _on_animated_sprite_2d_animation_changed() -> void:
 	if $AnimatedSprite2D.animation == "idle":
 		$AnimatedSprite2D.scale = Vector2(1, 1)
 	if $AnimatedSprite2D.animation == "running":
 		$AnimatedSprite2D.scale = Vector2(3, 3)
+
+func _process(_delta: float) -> void:
+	if position.x >= 10000:
+		done = true
