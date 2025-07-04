f2e9299488fb0a94cf425994faed5514c15a51e9
TimonDeBeste <123480186+TimonDeBeste@users.noreply.github.com>
2025-07-04 11:53:12 +0200

    JOSEEEEEEEEEEEEEEEEEEE

--- scripts/jose.gd
+++ scripts/jose.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-func _physics_process(delta) -> void:
+func _physics_process(_delta) -> void:
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = 5
 	if Input.is_action_pressed("ui_left"):
@@ -9,3 +9,9 @@ func _physics_process(delta) -> void:
 		velocity.x = 0
 	
 	move_and_slide()
+
+func _on_animated_sprite_2d_animation_changed() -> void:
+	if $AnimatedSprite2D.animation == "idle":
+		$AnimatedSprite2D.scale = Vector2(1, 1)
+	if $AnimatedSprite2D.animation == "running":
+		$AnimatedSprite2D.scale = Vector2(3, 3)
