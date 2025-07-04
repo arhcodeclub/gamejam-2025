86524833a5611fe328316b8f7eb25471808a2ffe
TetieWasTaken <tjvssr@gmail.com>
2025-07-04 16:22:50 +0200

    fix lose

--- scripts/jose.gd
+++ scripts/jose.gd
@@ -44,4 +44,4 @@ func _process(_delta: float) -> void:
 
 func _on_timer_timeout():
 	print("Change to menu")
-	get_tree().change_scene_to_file("res://WinorLose.tscn");
+	get_tree().change_scene_to_file("res://Lose.tscn");
