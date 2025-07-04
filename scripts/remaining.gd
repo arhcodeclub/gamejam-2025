1f813e09cb9660e5b1195a6018e275eec28fade3
TetieWasTaken <tjvssr@gmail.com>
2025-07-04 16:00:34 +0200

    alarm

--- scripts/remaining.gd
+++ scripts/remaining.gd
@@ -4,14 +4,20 @@ extends Label
 @onready var victory = $victory
 @onready var short_timer = $ShortTimer
 @onready var music = $"../../../music"
+@onready var alarm = $alarm
 
 var done = false
+var isPlaying = false
 
 func _ready():
 	timer.start()
 	
 func _process(delta):
 	text = "Time remaining: %ss" % round(timer.time_left)
+	
+	if timer.time_left <= 10 && isPlaying != true:
+		isPlaying = true
+		alarm.play()
 	
 	if timer.time_left <= 0 && done != true:
 		done = true
