f3ddcd40d0d3deb53bab93a41c402fab56acdec1
TimonDeBeste <123480186+TimonDeBeste@users.noreply.github.com>
2025-07-04 15:48:34 +0200

    Update trump.gd

--- scripts/trump.gd
+++ scripts/trump.gd
@@ -1,17 +1,17 @@
 extends Node2D
 
-@export var money_per_second :float = 7.93
+@export var money_per_second: float = 7.93
 
 @onready var global = $"../.."
+@onready var timer = $"../Control/remaining/Timer"
+@onready var sprite = $AnimatedSprite2D
 
 func _ready() -> void:
-	$Timer.set_wait_time(1)
-	$AnimatedSprite2D.play("normal")
+	sprite.play("normal")
 
 func _process(_delta: float) -> void:
-	if global.distance > 12050/2:
-		$AnimatedSprite2D.play("anger_2")
-
+	if round(timer.time_left) < 30:
+		sprite.play("anger_2")
 
 func _on_timer_timeout() -> void:
 	global.money += money_per_second
