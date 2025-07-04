ea292352e79a3f8022562e1f3db6eaf83662a3cb
TimonDeBeste <123480186+TimonDeBeste@users.noreply.github.com>
2025-07-04 12:15:04 +0200

    jose is nu fucking aan het bewegen

--- scripts/jose.gd
+++ scripts/jose.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
 
-func _physics_process(_delta) -> void:
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = 5
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -5
-	else:
-		velocity.x = 0
-	
-	move_and_slide()
+@export var speed : int = 100
+@export var gravity : int = 25
 
+func _ready() -> void:
+	$AnimatedSprite2D.play("running")
+
+func _physics_process(_delta) -> void:
+	velocity.x = speed
+	velocity.y += gravity
+	move_and_slide()
+	
 func _on_animated_sprite_2d_animation_changed() -> void:
 	if $AnimatedSprite2D.animation == "idle":
 		$AnimatedSprite2D.scale = Vector2(1, 1)
