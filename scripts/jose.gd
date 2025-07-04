677d78e9d90566b126740db6e752eee175d8bee9
DrDehydratedWater <karpadf@gmail.com>
2025-07-04 09:21:15 +0000

    simple movement left n' right

--- scripts/jose.gd
+++ scripts/jose.gd
@@ -1,1 +1,11 @@
 extends CharacterBody2D
+
+func _physics_process(delta) -> void:
+    if Input.is_action_pressed("ui_right"):
+        velocity.x = 5
+    if Input.is_action_pressed("ui_left"):
+        velocity.x = -5
+    else:
+        velocity.x = 0
+    
+    move_and_slide()
