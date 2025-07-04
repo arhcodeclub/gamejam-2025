93ce689ddfd54a13edc390d32c332c96a0335226
TimonDeBeste <123480186+TimonDeBeste@users.noreply.github.com>
2025-07-04 10:42:09 +0200

    Trump script

--- scripts/trump.gd
+++ scripts/trump.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+@export var money_per_second :float = 7.93
+
+func _ready() -> void:
+	$Timer.set_wait_time(1)
+
+
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_timer_timeout() -> void:
+	print("timeout")
