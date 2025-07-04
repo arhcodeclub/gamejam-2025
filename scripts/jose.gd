62cb15c9a89af2e91527c7f1686e41332ae65290
TimonDeBeste <123480186+TimonDeBeste@users.noreply.github.com>
2025-07-04 13:12:21 +0200

    poep

--- scripts/jose.gd
+++ scripts/jose.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@export var speed : int = 100
+@export var speed : int = 256
+@export var jump_speed : int = 100
 @export var gravity : int = 25
 
 func _ready() -> void:
@@ -10,8 +11,10 @@ func _ready() -> void:
 func _physics_process(_delta) -> void:
 	velocity.x = speed
 	velocity.y += gravity
+	if $RayCast2D.is_colliding():
+		velocity.y -= jump_speed
 	move_and_slide()
-	
+	print(velocity.y)
 func _on_animated_sprite_2d_animation_changed() -> void:
 	if $AnimatedSprite2D.animation == "idle":
 		$AnimatedSprite2D.scale = Vector2(1, 1)
