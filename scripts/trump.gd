072a845ccb16edc7fd62b709a8f1881800611e35
TimonDeBeste <123480186+TimonDeBeste@users.noreply.github.com>
2025-07-04 15:22:43 +0200

    Trump gefikst

--- scripts/trump.gd
+++ scripts/trump.gd
@@ -2,12 +2,15 @@ extends Node2D
 
 @export var money_per_second :float = 7.93
 
-func _ready() -> void:
-	$Timer.set_wait_time(1)
 @onready var global = $"../.."
 
+func _ready() -> void:
+	$Timer.set_wait_time(1)
+	$AnimatedSprite2D.play("normal")
+
 func _process(_delta: float) -> void:
-	pass
+	if global.distance > 12050/2:
+		$AnimatedSprite2D.play("anger_2")
 
 
 func _on_timer_timeout() -> void:
