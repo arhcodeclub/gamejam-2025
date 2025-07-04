b681cc33aabcd8699176573934b39e78ce637eaa
TetieWasTaken <tjvssr@gmail.com>
2025-07-04 13:58:10 +0200

    timer

--- scripts/remaining.gd
+++ scripts/remaining.gd
@@ -7,3 +7,6 @@ func _ready():
 	
 func _process(delta):
 	text = "Time remaining: %ss" % round(timer.time_left)
+	
+	if timer.time_left <= 0:
+		get_tree().change_scene_to_file("res://scenes/Mainmenu.tscn");
